62e42b66a92afaee7e1933e1a8fd74d2442c28a6
tim-salabim <tim.appelhans@gmail.com>
2014-11-18 16:04:12 +0100

    initial commit

---
+++ R/fPattern.R
@@ -0,0 +1,11 @@
+fPattern <- function(x){
+  Avg_Temp_Image <- cellStats(x, stat='mean')
+  
+  fpattern <- stack(lapply(seq(nlayers(x)), function(i) {
+    tst <- x[[i]] - as.numeric(Avg_Temp_Image[i])  
+    return(tst)
+  }))
+  return(fpattern)
+}
+  
+ 
